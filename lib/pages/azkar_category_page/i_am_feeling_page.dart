47c5ce3afebafb82f9ba8c8fda5ed89484c1994e
Ammar_Ahmad <ammar.salim2018@gmail.com>
2024-10-07 14:30:59 +0300

    rework

--- lib/pages/azkar_category_page/i_am_feeling_page.dart
+++ lib/pages/azkar_category_page/i_am_feeling_page.dart
@@ -5,6 +5,10 @@ class DuaaPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('I am feeling'),
+      ),
+    );
   }
 }
